198f5dc1b2e24727e528f9f61d19d01b8f401a07
nadopro <nadopro@gmail.com>
2025-11-05 15:57:29 +0900

    C1558

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
     $SMSID = "smsfb";
     $SECURENO = "시큐어키 등록";
-    $ReceiveMobile = "010-1111-2222";// 받는 사람
-    $sms_url = "https://sslsms.cafe24.com/sms_sender.php"; 
+    $ReceiveMobile = "010-1111-2222";// 받는 사람 
     $MOBILE1 = "1111";
     $MOBILE2 = "1111"; 
 
